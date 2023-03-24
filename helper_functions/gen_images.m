b7e02618211e866898e2812bd893efc49b96e21c
Haixin <46263698+HaixinLiuNeuro@users.noreply.github.com>
2023-03-24 14:35:10 -0400

    Update gen_images.m
    exclude overlapping ROI in the label image

--- helper_functions/gen_images.m
+++ helper_functions/gen_images.m
@@ -22,10 +22,29 @@ im_l = zeros(im_info.height,im_info.width, 'uint8');
 
 % loop through cells 
 for i_cell = 1:im_info.num_cell
+    %% handle overlapped ROIs by subtract any overlapping pixel with current im_l
+    %{
+    use a tracker to track overlapped pixels by multiple ROIs, for the target
+    ROI and mask subtract the overlapping areas.
+    ROIs and masks will be saved for CNN training purpose
+    %}
+    tmp_im_l_add = uint8(poly2mask(ROIs.cell_roi_xy(:,1,i_cell),ROIs.cell_roi_xy(:,2,i_cell),im_info.width,im_info.height)); %.* uint8(i_cell);
+    tmp_im_l_ol_track = uint8(( uint8(im_f > 0) + tmp_im_l_add ) > 1); % use the made image to track any pixel ocuppied already
+    tmp_im_l_add = uint8(tmp_im_l_add - tmp_im_l_ol_track) .* uint8(i_cell);
+    % add the new cell without the overlapping part
+    im_l = im_l + tmp_im_l_add; 
+    % make overlapping part to zero on the labeled image
+    if nnz(tmp_im_l_ol_track)>0
+        im_l(tmp_im_l_ol_track>0) = uint8(0);
+    end
+   
+    %% make current image (do mask first since using im_f to track occupied pixels
     im_f = im_f ...
         +  poly2mask(ROIs.cell_roi_xy(:,1,i_cell),ROIs.cell_roi_xy(:,2,i_cell),im_info.width,im_info.height) .* im_info.f_intensity_cell(i_cell) ... % cytoplasma
         -  poly2mask(ROIs.nuc_roi_xy(:,1,i_cell), ROIs.nuc_roi_xy(:,2,i_cell), im_info.width,im_info.height) .* im_info.f_intensity_cell(i_cell) .* nucleus_lit; % lighter nucleus
-    im_l = im_l + uint8(poly2mask(ROIs.cell_roi_xy(:,1,i_cell),ROIs.cell_roi_xy(:,2,i_cell),im_info.width,im_info.height)).* uint8(i_cell);
+
+
+    
 end
 im_f = uint16(im_f);
 params = [];
